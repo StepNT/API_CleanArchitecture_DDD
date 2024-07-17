267f90cad259f6a5a267e7c6ba0e7353473ea1dc
sompob.s <sompob.s@thaicreditbank.com>
2024-07-17 11:15:49 +0700

    inti

---
+++ CleanArchitectureDDD-master/Domain/Entities/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Domain.Entities;
+
+public partial class Customer
+{
+    [Key]
+    public Guid ID { get; set; }
+
+    public string? Code { get; set; }
+
+    public string? Name { get; set; }
+
+    public short? Age { get; set; }
+
+    public string? Email { get; set; }
+
+    public bool? IsActive { get; set; }
+}
---
+++ CleanArchitectureDDD-master/Infrastructure/Abstractions/Dapper/DapperConnection.cs
@@ -0,0 +1,23 @@
+using System.Data;
+using Domain.Interfaces;
+using Microsoft.Data.SqlClient;
+
+namespace Infrastructure.Abstractions.Dapper;
+
+internal sealed class DapperConnection : IDapperConnection
+{
+    private readonly string _connectionString;
+
+    public DapperConnection(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public IDbConnection CreateConnection()
+    {
+        var connection = new SqlConnection(_connectionString);
+        connection.Open();
+
+        return connection;
+    }
+}
